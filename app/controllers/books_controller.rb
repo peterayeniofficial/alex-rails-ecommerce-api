f2211097578e2165c09c8526d0e3629fb4f1897c
Peter Ayeni <ayenicology@gmail.com>
2019-11-09 14:04:31 +0000

    Add fields selector to Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,12 @@
+# app/controllers/books_controller.rb
 class BooksController < ApplicationController
 
   def index
-    render json: { data: Book.all }
+    books = Book.all.map do |book|
+      FieldPicker.new(BookPresenter.new(book, params)).pick
+    end
+
+    render json: { data: books }.to_json
   end
 
 end
