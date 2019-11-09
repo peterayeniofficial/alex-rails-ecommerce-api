d20d6668431dab0a828923a6e31309bca244b7d5
Peter Ayeni <ayenicology@gmail.com>
2019-11-09 11:16:03 +0000

    Finished Models

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,7 @@ class Book < ApplicationRecord
 
   validates :isbn_10, presence: true, length: { is: 10 }, uniqueness: true
   validates :isbn_13, presence: true, length: { is: 13 }, uniqueness: true
+
+  mount_base64_uploader :cover, CoverUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191109102309) do
+ActiveRecord::Schema.define(version: 20191109110445) do
 
   create_table "authors", force: :cascade do |t|
     t.string "given_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20191109102309) do
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cover"
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
   end
